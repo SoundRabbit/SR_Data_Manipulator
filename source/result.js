a75b40d2c519c1cca019f3408c889604680941a1
neunyan <soundrabbitstd@gmail.com>
2019-08-06 18:40:48 +0900

    appned map, andThen, method-chain to result

--- source/result.js
+++ source/result.js
@@ -3,11 +3,44 @@ const { match } = require("./match");
 
 const Result = new Enum("Ok", "Err");
 
+const $ = result => ({
+    map: proc => $(map(proc)(result)),
+    andThen: proc => $(andThen(proc)(result)),
+    withDefault: val => withDefault(val)(result),
+    promise: () => promise(result),
+    boolean: () => boolean(result)
+});
+
+const map = proc => result =>
+    match(result).with({
+        [Result.$Ok]: val => new Result.Ok(util.maybeFunction(proc)(val)),
+        [Result.$Err]: util.always(result)
+    });
+
+const andThen = proc => result =>
+    match(result).with({
+        [Result.$Ok]: val => util.maybeFunction(proc)(val),
+        [Result.$Err]: util.always(result)
+    });
+
+const withDefault = val => result =>
+    match(result).with({
+        [Result.$Ok]: val => util.maybeFunction(proc)(val),
+        [Result.$Err]: util.always(val)
+    });
+
 const promise = result =>
     match(result).with({
         [Result.$Ok]: val => Promise.resolve(val),
         [Result.$Err]: _ => Promise.reject()
     });
+
+const boolean = result =>
+    match(result).with({
+        [Result.$Ok]: util.always(true),
+        [Result.$Err]: util.always(false)
+    });
+
 module.exports = {
     Result,
     promise
