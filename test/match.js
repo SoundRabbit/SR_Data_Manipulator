6282ff425a39e5c40280bfc96957f248a29f80eb
neunyan <soundrabbitstd@gmail.com>
2019-08-23 10:44:09 +0900

    modify test

--- test/match.js
+++ test/match.js
@@ -67,4 +67,32 @@ describe("#match", () => {
             assert.equal(res, 13);
         });
     });
+    describe("pattern", () => {
+        it("should 3", () => {
+            const a = new E.a(1);
+            const res = match(a)([E.a(), $ => $ + 2], [match.default, $ => $]);
+            assert.equal(res, 3);
+        });
+        it("should 5", () => {
+            const a = new E.a(5);
+            const res = match(a)([E.b(), $ => $ + 2], [match.default, $ => $]);
+            assert.equal(res, 5);
+        });
+        it("should 8", async () => {
+            const a = new E.a(6);
+            const res = await match(a)(
+                [E.a(), async $ => (await promiseFunc($)) + 2],
+                [match.default, async $ => $]
+            );
+            assert.equal(res, 8);
+        });
+        it("should 13", async () => {
+            const a = new E.a(13);
+            const res = await match(a)(
+                [E.b(), async $ => $ + 8],
+                [match.default, async $ => $]
+            );
+            assert.equal(res, 13);
+        });
+    });
 });
